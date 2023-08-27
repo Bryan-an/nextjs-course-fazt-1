6cc45d1c6bad799719a58f427fa7513854fd27ea
Bryan-an <bryanandagoya@gmail.com>
2023-08-27 17:34:26 -0500

    fetch data

---
+++ app/posts/page.jsx
@@ -0,0 +1,24 @@
+import PostCard from '@/components/post-card';
+import React from 'react';
+
+const loadPosts = async () => {
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+  const data = await res.json();
+  return data;
+};
+
+// RSC
+const PostsPage = async () => {
+  const posts = await loadPosts();
+  console.log(posts);
+
+  return (
+    <div>
+      {posts.map((post) => (
+        <PostCard key={post.id} post={post} />
+      ))}
+    </div>
+  );
+};
+
+export default PostsPage;
---
+++ components/post-card.jsx
@@ -0,0 +1,22 @@
+'use client';
+
+// RCC
+const PostCard = ({ post }) => {
+  return (
+    <div>
+      <h3>
+        {post.id}. {post.title}
+      </h3>
+      <p>{post.body}</p>
+      <button
+        onClick={() => {
+          alert('click working');
+        }}
+      >
+        click
+      </button>
+    </div>
+  );
+};
+
+export default PostCard;
