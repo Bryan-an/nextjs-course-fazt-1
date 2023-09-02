288ab68f0e4788747ff6b66757335a9eda928047
Bryan-an <bryanandagoya@gmail.com>
2023-09-02 15:33:01 -0500

    route handlers

---
+++ src/app/api/users/route.js
@@ -0,0 +1,19 @@
+import { NextResponse } from 'next/server';
+
+export const GET = async () => {
+  const res = await fetch('https://jsonplaceholder.typicode.com/users');
+  const data = await res.json();
+  return NextResponse.json({ users: data });
+};
+
+export const POST = () => {
+  return NextResponse.json({ message: 'create data!' });
+};
+
+export const PUT = () => {
+  return NextResponse.json({ message: 'update data!' });
+};
+
+export const DELETE = () => {
+  return NextResponse.json({ message: 'delete data!' });
+};
